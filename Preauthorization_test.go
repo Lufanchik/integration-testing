fd24db723f54823e92592bdff7fea18375b1b436
pyreuis <ilushapyreu@gmail.com>
2019-11-07 11:54:26 +0300

    Testing

---
+++ Preauthorization_test.go
@@ -0,0 +1,63 @@
+package http_test
+
+import (
+	"lab.siroccotechnology.ru/tp/common/messages/carriers"
+	"testing"
+)
+
+var (
+	casesSimplePreauthorization = Cases{
+		{
+			&Pass{
+				PaymentType: PaymentTypeFullPayment,
+				RequestType: RequestTypeOnline,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				ExpectedSum: 4200,
+				AuthType:    AuthTypeIncorrect,
+			},
+		},
+	}
+)
+
+func TestSimplePreauthorization(t *testing.T) {
+	Run(t, casesSimplePreauthorization)
+}
+
+var (
+	casesSimplePreauthorizationone = Cases{
+		{
+			&Pass{
+				PaymentType: PaymentTypeFullPayment,
+				RequestType: RequestTypeOnline,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				ExpectedSum: 4200,
+				AuthType:    AuthTypeIncorrect,
+			},
+		},
+	}
+)
+
+func TestSimplePreauthorizationone(t *testing.T) {
+	Run(t, casesSimplePreauthorizationone)
+}
+
+var (
+	casesSimplePreauthorizationtwo = Cases{
+		{
+			&Pass{
+				PaymentType: PaymentTypeFullPayment,
+				RequestType: RequestTypeOnline,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				ExpectedSum: 4200,
+				AuthType:    AuthTypeIncorrect,
+			},
+		},
+	}
+)
+
+func TestSimplePreauthorizationtwo(t *testing.T) {
+	Run(t, casesSimplePreauthorizationtwo)
+}
--- config_test.go
+++ config_test.go
@@ -1,8 +1,8 @@
 package http_test
 
 const (
-	processingApiUrl  = "http://10.244.2.108:9090"
-	passUrl           = "http://10.244.2.99:13380"
+	processingApiUrl  = "http://10.244.3.40:9090"
+	passUrl           = "http://10.244.1.53:13380"
 	apmApiUrl         = "http://127.0.0.1:1340"
 	globalRequestType = RequestTypeOnline
 )

191e87bbe40985fd4f74eddef83e7fccdba1da74
pyreuis <ilushapyreu@gmail.com>
2019-11-08 11:54:48 +0300

    Testing

--- config_test.go
+++ config_test.go
@@ -2,7 +2,7 @@ package http_test
 
 const (
 	processingApiUrl  = "http://10.244.3.40:9090"
-	passUrl           = "http://10.244.2.183:13380"
+	passUrl           = "http://10.244.2.30:13380"
 	apmApiUrl         = "http://127.0.0.1:1340"
 	globalRequestType = RequestTypeOnline
 )

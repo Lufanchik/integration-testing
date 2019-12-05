1018e167968c391edc5a41f7eed74fafe412b880
gusmanoveo <gusmanoveo@multicarta.ru>
2019-12-05 10:12:26 +0300

    test case commit

--- passes/mck/metro_complex_mck_time.go
+++ passes/mck/metro_complex_mck_time.go
@@ -23,51 +23,49 @@ var CasesComplexTimeMCK = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2000155", //ФИЛИ
+					Station:   "2000155", //ФИЛИ
 					Direction: processing.TerminalDirection_INGRESS,
 				},
-				Parent: 1,
-				Now:     test.NowCustom(10, 20),
-				TimeToWait:  10 * time.Minute,
+				Parent:     1,
+				Now:        test.NowCustom(10, 20),
+				TimeToWait: 10 * time.Minute,
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypeFree,
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2001270", //ОКРУЖНАЯ
+					Station:   "2001270", //ОКРУЖНАЯ
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 2,
 				Now:     test.NowCustom(11, 00),
-
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypeFree,
 				Carrier:     carriers.Carrier_MM,
 				SubCarrier:  carriers.SubCarrier_MM_SUB,
-				Parent: 1,
+				Parent:      1,
 				Now:         test.NowCustom(11, 01),
-
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypeFree,
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD2_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2002780", //ДЕПО
+					Station:   "2002780", //ДЕПО
 					Direction: processing.TerminalDirection_INGRESS,
 				},
-				Parent: 1,
-				Now:     test.NowCustom(11, 02),
-				TimeToWait:  8 * time.Minute,
+				Parent:     1,
+				Now:        test.NowCustom(11, 02),
+				TimeToWait: 8 * time.Minute,
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypeFree,
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD2_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2000045", //ТЕКСТИЛЬЩИКИ
+					Station:   "2000045", //ТЕКСТИЛЬЩИКИ
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 5,
@@ -78,12 +76,12 @@ var CasesComplexTimeMCK = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD2_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2000075", //ТУШИНО
+					Station:   "2000075", //ТУШИНО
 					Direction: processing.TerminalDirection_INGRESS,
 				},
 				ExpectedSum: 4200,
-				Now:        test.NowCustom(15, 00),
-				TimeToWait: 5 * time.Minute,
+				Now:         test.NowCustom(15, 00),
+				TimeToWait:  5 * time.Minute,
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypePayment,
@@ -93,10 +91,9 @@ var CasesComplexTimeMCK = test.Cases{
 					Station:   "2000460", //НАХАБИНО
 					Direction: processing.TerminalDirection_EGRESS,
 				},
-				Now:   	 	 test.NowCustom(15, 33 ),
-				Ingress: 7,
+				Now:         test.NowCustom(15, 33),
+				Ingress:     7,
 				ExpectedSum: 700,
-
 			},
 		},
 	},
@@ -153,12 +150,12 @@ var CasesComplexTimeMCK = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD2_MSK,
 				Terminal: &processing.Terminal{
-					Station: "2000075", //ТУШИНО
+					Station:   "2000075", //ТУШИНО
 					Direction: processing.TerminalDirection_INGRESS,
 				},
 				ExpectedSum: 4200,
-				Now:        test.NowCustom(15, 00),
-				TimeToWait: 5 * time.Minute,
+				Now:         test.NowCustom(15, 00),
+				TimeToWait:  5 * time.Minute,
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypePayment,
@@ -168,10 +165,88 @@ var CasesComplexTimeMCK = test.Cases{
 					Station:   "2000460", //НАХАБИНО
 					Direction: processing.TerminalDirection_EGRESS,
 				},
-				Now:   	 	 test.NowCustom(15, 33 ),
-				Ingress: 6,
+				Now:         test.NowCustom(15, 33),
+				Ingress:     6,
 				ExpectedSum: 700,
 			},
 		},
 	},
+
+	{
+		N: "3. MCD2_MO - MCK - MM - MMTS", //поездка началась через 4ч:59м после первого тапа
+		T: test.T{
+			&test.Pass{
+				PaymentType: test.PaymentTypePayment,
+				Carrier:     carriers.Carrier_MCD,
+				SubCarrier:  carriers.SubCarrier_MCD2_MO,
+				Terminal: &processing.Terminal{
+					Station:   "2000065", //ПОДОЛЬСК
+					Direction: processing.TerminalDirection_INGRESS,
+				},
+				Now:         test.NowCustom(10, 00),
+				ExpectedSum: 4900,
+				TimeToWait:  300 * time.Minute,
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypeFree,
+				Carrier:     carriers.Carrier_MCD,
+				SubCarrier:  carriers.SubCarrier_MCD2_MSK,
+				Terminal: &processing.Terminal{
+					Station:   "2002780", //ДЕПО
+					Direction: processing.TerminalDirection_EGRESS,
+				},
+				Ingress: 1,
+				Now:     test.NowCustom(15, 39),
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypeFree,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				Now:         test.NowCustom(15, 44),
+				Parent:      1,
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypeFree,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				Now:         test.NowCustom(15, 50),
+				Parent:      1,
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypeFree,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				Now:         test.NowCustom(16, 01),
+				Parent:      1,
+			},
+		},
+	},
+
+	//{
+	//	N: "4. MCK - MM - MMTS",
+	//	T: test.T{
+	//		&test.Pass{
+	//			PaymentType: test.PaymentTypePayment,
+	//			Carrier:     carriers.Carrier_MM,
+	//			SubCarrier:  carriers.SubCarrier_MCK_SUB,
+	//			ExpectedSum: 4200,
+	//			Now:         test.NowCustom(10, 00),
+	//		},
+	//		&test.Pass{
+	//			PaymentType: test.PaymentTypeFree,
+	//			Carrier:     carriers.Carrier_MM,
+	//			SubCarrier:  carriers.SubCarrier_MM_SUB,
+	//			ExpectedSum: 4200,
+	//			Parent: 1,
+	//			Now:         test.NowCustom(11, 29),
+	//		},
+	//		&test.Pass{
+	//			PaymentType: test.PaymentTypePayment,
+	//			Carrier:     carriers.Carrier_MM,
+	//			SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+	//			ExpectedSum: 4200,
+	//			Now:         test.NowCustom(13, 02),
+	//		},
+	//	},
+	//},
 }

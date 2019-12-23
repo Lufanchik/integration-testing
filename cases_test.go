44e6c4acdb4b06f5c9a414db69b260ebaea45458
csl <chvalovsl@multicarta.ru>
2019-12-23 15:19:14 +0300

    Fix

--- cases_test.go
+++ cases_test.go
@@ -3,65 +3,61 @@ package integration_testing
 import (
 	"lab.siroccotechnology.ru/tp/integration-testing/apm"
 	"lab.siroccotechnology.ru/tp/integration-testing/parking"
-	//"lab.siroccotechnology.ru/tp/integration-testing/passes"
-	//"lab.siroccotechnology.ru/tp/integration-testing/passes/mcd"
-	//"lab.siroccotechnology.ru/tp/integration-testing/passes/mck"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes/mcd"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes/mck"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes/mgt"
-	//"lab.siroccotechnology.ru/tp/integration-testing/passes/mm"
-	//"lab.siroccotechnology.ru/tp/integration-testing/passes/mmts"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes/mm"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes/mmts"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes/mtppk"
-	//"lab.siroccotechnology.ru/tp/integration-testing/test"
 	"lab.siroccotechnology.ru/tp/integration-testing/webapi"
 )
 
 func init() {
 	//параллельный запуск
 	//общие кейсы проходов
-	//AddP(passes.CasesCancel)
-	//AddP(passes.CasesSimpleComplexPass)
+	AddP(passes.CasesCancel)
+	AddP(passes.CasesSimpleComplexPass)
 
 	//МТППК
-	//AddP(mtppk.CasesMTPPKPasses)
+	AddP(mtppk.CasesMTPPKPasses)
+	//AddP(mtppk.CasesMMTSMTPPK)
 
 	//ММТС
-	//AddP(mmts.CasesComplexPassMMTS1)
-	//AddP(mmts.CasesComplexPassMMTS2)
-	//AddP(mmts.CasesComplexPassMMTS3)
-	//AddP(mmts.CasesComplexPassMMTS4)
-	//AddP(mmts.CasesComplexPassMMTS5)
-	//AddP(mmts.CasesOfflineMetroComplexMMTS)
+	AddP(mmts.CasesComplexPassMMTS1)
+	AddP(mmts.CasesComplexPassMMTS2)
+	AddP(mmts.CasesComplexPassMMTS3)
+	AddP(mmts.CasesComplexPassMMTS4)
+	AddP(mmts.CasesComplexPassMMTS5)
+	AddP(mmts.CasesOfflineMetroComplexMMTS)
 
 	//MM
-	//AddP(mm.CasesMetroComplexMM1)
-	//AddP(mm.CasesMetroComplexMM2)
-	//AddP(mm.CasesMetroComplexMM3)
-	//AddP(mm.CasesMetroComplexMM4)
-	//AddP(mm.CasesOfflineMetroComplexMM)
-	//
-	////МЦК
-	//AddP(mck.CasesMetroComplexMCK1)
-	//AddP(mck.CasesMetroComplexMCK2)
-	//AddP(mck.CasesMetroComplexMCK3)
-	//AddP(mck.CasesMetroComplexMCK4)
-	//AddP(mck.CasesOfflineMetroComplexMCK)
-	//
-	////МЦД
-	//AddP(mcd.CasesMetroComplexMCDMSK1)
-	//AddP(mcd.CasesMetroComplexMCDMSK2)
-	//AddP(mcd.CasesMetroComplexMCDMSK3)
-	//AddP(mcd.CasesMetroComplexMCDMSK4)
-	//AddP(mcd.CasesMetroComplexMCDMSK5)
-	//AddP(mcd.CasesComplexMCDMOPartOne)
-	//AddP(mcd.CasesComplexMCDMOPartTwo)
-	//AddP(mcd.CasesComplexMCDMOPartThree)
-	//AddP(mcd.CasesComplexMCDMOPartFour)
-	//AddP(mcd.CasesComplexMCDMOPartFife)
-	//AddP(mcd.CasesOfflineMetroComplexMCD)
-	//AddP(mcd.CasesOfflineMetroComplexMCDMO)
+	AddP(mm.CasesMetroComplexMM1)
+	AddP(mm.CasesMetroComplexMM2)
+	AddP(mm.CasesMetroComplexMM3)
+	AddP(mm.CasesMetroComplexMM4)
+	AddP(mm.CasesOfflineMetroComplexMM)
 
+	//МЦК
+	AddP(mck.CasesMetroComplexMCK1)
+	AddP(mck.CasesMetroComplexMCK2)
+	AddP(mck.CasesMetroComplexMCK3)
+	AddP(mck.CasesMetroComplexMCK4)
+	AddP(mck.CasesOfflineMetroComplexMCK)
 
-	//MTPPK
-	Add(mtppk.CasesMMTSMTPPK)
+	//МЦД
+	AddP(mcd.CasesMetroComplexMCDMSK1)
+	AddP(mcd.CasesMetroComplexMCDMSK2)
+	AddP(mcd.CasesMetroComplexMCDMSK3)
+	AddP(mcd.CasesMetroComplexMCDMSK4)
+	AddP(mcd.CasesMetroComplexMCDMSK5)
+	AddP(mcd.CasesComplexMCDMOPartOne)
+	AddP(mcd.CasesComplexMCDMOPartTwo)
+	AddP(mcd.CasesComplexMCDMOPartThree)
+	AddP(mcd.CasesComplexMCDMOPartFour)
+	AddP(mcd.CasesComplexMCDMOPartFife)
+	AddP(mcd.CasesOfflineMetroComplexMCD)
+	AddP(mcd.CasesOfflineMetroComplexMCDMO)
 
 	//APM
 	AddP(apm.CasesApmGateway)
@@ -84,8 +80,4 @@ func init() {
 	//AddP(mcd.CasesComplexTimeMCD)
 	//Add(mmts.CasesComplexTimeMMTS)
 	//AddP(mcd.CasesComplexTimeMCD)
-}
-
-func Add(cases mtppk.CasesMMTSMTPPK) {
-
 }

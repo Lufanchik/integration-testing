9fdc39f1088432673f8263dc7711005bb74073aa
Aleksandr Moshkin <moshkin_aleksandr@icloud.com>
2019-12-30 18:20:27 +0300

    faces merge

--- passes/face/face.go
+++ passes/face/face.go
@@ -9,7 +9,24 @@ import (
 
 var CasesAuthWithFace = test.Cases{
 	{
-		N:          "Face Auth Test",
+		N:          "1. Face Auth Test / mgt",
+		CardSystem: processing.CardSystem_VISA,
+		FaceId:     uuid.New().String(),
+		T: test.T{
+			&test.RegisterFaceId{},
+			&test.Pass{
+				PaymentType: test.PaymentTypePayment,
+				Carrier:     carriers.Carrier_MGT,
+				ExpectedSum: 4200,
+			},
+			&test.FacePass{
+				CreateLinks: []int{1},
+				Passes:      []int{2},
+			},
+		},
+	},
+	{
+		N:          "2. Face Auth Test / mm",
 		CardSystem: processing.CardSystem_VISA,
 		FaceId:     uuid.New().String(),
 		T: test.T{
@@ -20,8 +37,67 @@ var CasesAuthWithFace = test.Cases{
 				SubCarrier:  carriers.SubCarrier_MM_SUB,
 				ExpectedSum: 4200,
 			},
-			&test.WebAPIPasses{
-				Passes: []int{2},
+			&test.FacePass{
+				CreateLinks: []int{1},
+				Passes:      []int{2},
+			},
+		},
+	},
+	{
+		N:          "3. Face Auth Test / mck",
+		CardSystem: processing.CardSystem_VISA,
+		FaceId:     uuid.New().String(),
+		T: test.T{
+			&test.RegisterFaceId{},
+			&test.Pass{
+				PaymentType: test.PaymentTypePayment,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				ExpectedSum: 4200,
+			},
+			&test.FacePass{
+				CreateLinks: []int{1},
+				Passes:      []int{2},
+			},
+		},
+	},
+	{
+		N:          "4. Face Auth Test / mmts",
+		CardSystem: processing.CardSystem_VISA,
+		FaceId:     uuid.New().String(),
+		T: test.T{
+			&test.RegisterFaceId{},
+			&test.Pass{
+				PaymentType: test.PaymentTypePayment,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				ExpectedSum: 4200,
+			},
+			&test.FacePass{
+				CreateLinks: []int{1},
+				Passes:      []int{2},
+			},
+		},
+	},
+	{
+		N:          "4. Face Auth Test / mcd_mo",
+		CardSystem: processing.CardSystem_VISA,
+		FaceId:     uuid.New().String(),
+		T: test.T{
+			&test.RegisterFaceId{},
+			&test.Pass{
+				PaymentType: test.PaymentTypePayment,
+				Carrier:     carriers.Carrier_MCD,
+				SubCarrier:  carriers.SubCarrier_MCD1_MO,
+				Terminal: &processing.Terminal{
+					Station:   "2000055", //ОДИНЦОВО
+					Direction: processing.TerminalDirection_INGRESS,
+				},
+				ExpectedSum: 4900,
+			},
+			&test.FacePass{
+				CreateLinks: []int{1},
+				Passes:      []int{2},
 			},
 		},
 	},

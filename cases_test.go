5d71a3ed8788205f4a27a0b5edc565213bc6095a
metelkinav <metelkinav@multicarta.ru>
2020-03-16 17:44:53 +0300

    Правки/поправки

--- cases_test.go
+++ cases_test.go
@@ -4,6 +4,7 @@ import (
 	"lab.siroccotechnology.ru/tp/integration-testing/apm"
 	"lab.siroccotechnology.ru/tp/integration-testing/parking"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes"
+	"lab.siroccotechnology.ru/tp/integration-testing/passes/face"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes/mcd"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes/mck"
 	"lab.siroccotechnology.ru/tp/integration-testing/passes/mgt"
@@ -82,7 +83,7 @@ func init() {
 	//Resolve
 	AddAR(registry.CasesResolveGetTaskList)
 	//Face
-	//AddP(face.CasesAuthWithFace)
+	AddP(face.CasesAuthWithFace)
 
 	//черновики, которые пока что не запускаются
 	//AddP(passes.CasesWrongTimeComplexPass)

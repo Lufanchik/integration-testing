385cba265fd42eab21c5ef26b5976485594edc4a
metelkinav <metelkinav@multicarta.ru>
2020-03-23 11:24:53 +0300

    + Mt cases

---
+++ passes/mt/passes.go
@@ -0,0 +1,25 @@
+package mt
+
+import (
+	"lab.siroccotechnology.ru/tp/common/messages/carriers"
+	"lab.siroccotechnology.ru/tp/integration-testing/test"
+)
+
+var CasesMetroComplexMT = test.Cases{
+	{
+		N: "1.MМ-MМ",
+		//PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				PaymentType: test.PaymentTypePrepayed,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypePrepayed,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+			},
+		},
+	},
+}

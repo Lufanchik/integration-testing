569ad91182b4dd9f50880be07bb987168ba3e3f0
metelkinav <metelkinav@multicarta.ru>
2020-04-03 11:57:17 +0300

    правки/поправки (коды станций)

--- passes/mm/metro_complex_mm2.go
+++ passes/mm/metro_complex_mm2.go
@@ -1047,7 +1047,7 @@ var CasesMetroComplexMM2 = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station:   "2002910", //НЕМЧИНОВКА
+					Station:   "2000275", //
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 2,
@@ -1094,7 +1094,7 @@ var CasesMetroComplexMM2 = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station:   "2002910", //НЕМЧИНОВКА
+					Station:   "2000275", //
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 2,
@@ -1141,7 +1141,7 @@ var CasesMetroComplexMM2 = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station:   "2002910", //НЕМЧИНОВКА
+					Station:   "2000275", //
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 2,
@@ -1206,11 +1206,11 @@ var CasesMetroComplexMM2 = test.Cases{
 				Parent:      1,
 			},
 			&test.Pass{
-				PaymentType: test.PaymentTypeFree,
+				PaymentType: test.PaymentTypePayment,
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station:   "2001140", //КУНЦЕВСКАЯ
+					Station:   "2002077", //
 					Direction: processing.TerminalDirection_INGRESS,
 				},
 			},
@@ -1219,7 +1219,7 @@ var CasesMetroComplexMM2 = test.Cases{
 				Carrier:     carriers.Carrier_MCD,
 				SubCarrier:  carriers.SubCarrier_MCD1_MSK,
 				Terminal: &processing.Terminal{
-					Station:   "2002910", //НЕМЧИНОВКА
+					Station:   "2000275", //
 					Direction: processing.TerminalDirection_EGRESS,
 				},
 				Ingress: 6,

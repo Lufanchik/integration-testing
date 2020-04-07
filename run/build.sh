5e9a79317b5c01eef51ae39f0319dda9c1328ec5
its.crowd@gmail.com <its.crowd@gmail.com>
2020-04-07 13:31:56 +0300

    tests

---
+++ run/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./apm-api-gateway.run && ./auth-service.run && ./calculator-service.run && ./mk-emulator.run && ./pass-service.run && \
+./processing-api-gateway.run && ./request-service.run && ./tidpool-service.run && ./user-service.run && ./web-api-gateway.run && \
+./resolve-service.run && ./revise-service.run && ./twpg-service.run

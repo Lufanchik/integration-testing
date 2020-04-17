bcae1ccf46ae9ff54da7961cb4cd25cf1d5a31dd
Aleksandr Moshkin <moshkin_aleksandr@icloud.com>
2020-04-17 10:22:04 +0300

    TWPG Reverse Order

--- cases_test.go
+++ cases_test.go
@@ -1,77 +1,93 @@
 package integration_testing
 
-import "lab.dt.multicarta.ru/tp/integration-testing/twpg"
+import (
+	"lab.dt.multicarta.ru/tp/integration-testing/apm"
+	"lab.dt.multicarta.ru/tp/integration-testing/parking"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/face"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mcd"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mck"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mgt"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mm"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mmts"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mt"
+	"lab.dt.multicarta.ru/tp/integration-testing/passes/mtppk"
+	"lab.dt.multicarta.ru/tp/integration-testing/registry"
+	"lab.dt.multicarta.ru/tp/integration-testing/twpg"
+	"lab.dt.multicarta.ru/tp/integration-testing/webapi"
+)
 
 func init() {
 	//параллельный запуск
 	//общие кейсы проходов
 	//CasesCancel 25 26 27 пока на доработке
-	//AddP(passes.CasesCancel)
-	//
-	////МТППК
-	//AddP(mtppk.CasesMTPPKPasses)
-	//AddP(mtppk.CasesMTPPK_MGT)
-	//AddP(mtppk.CasesMTPPK_MCD_MO)
-	//AddP(mtppk.CasesMTPPK_MCK)
-	//AddP(mtppk.CasesMTPPK_MCD_MSK)
-	//AddP(mtppk.CasesMTPPK_single)
-	//
-	////ММТС
-	//AddP(mmts.CasesComplexPassMMTS1)
-	//AddP(mmts.CasesComplexPassMMTS2)
-	//AddP(mmts.CasesComplexPassMMTS3)
-	//AddP(mmts.CasesComplexPassMMTS4)
-	//AddP(mmts.CasesComplexPassMMTS5)
-	//
-	////MM
-	//AddP(mm.CasesMetroComplexMM1)
-	//AddP(mm.CasesMetroComplexMM2)
-	//AddP(mm.CasesMetroComplexMM3)
-	//AddP(mm.CasesMetroComplexMM4)
-	//
-	////МЦК
-	//AddP(mck.CasesMetroComplexMCK1)
-	//AddP(mck.CasesMetroComplexMCK2)
-	//AddP(mck.CasesMetroComplexMCK3)
-	//AddP(mck.CasesMetroComplexMCK4)
-	//
-	////МЦД
-	//AddP(mcd.CasesMetroComplexMCDMSK1)
-	//AddP(mcd.CasesMetroComplexMCDMSK2)
-	//AddP(mcd.CasesMetroComplexMCDMSK3)
-	//AddP(mcd.CasesMetroComplexMCDMSK4)
-	//AddP(mcd.CasesMetroComplexMCDMSK5)
-	//AddP(mcd.CasesComplexMCDMOPartOne)
-	//AddP(mcd.CasesComplexMCDMOPartTwo)
-	//AddP(mcd.CasesComplexMCDMOPartThree)
-	//AddP(mcd.CasesComplexMCDMOPartFour)
-	//AddP(mcd.CasesComplexMCDMOPartFife)
-	//
-	////Мобильная тройка
-	//AddP(mt.CasesMetroComplexMT)
-	//
-	////APM
-	//AddP(apm.CasesApmGateway)
-	//
-	////МГТ
-	//AddP(mgt.CasesMGT_1)
-	//AddP(mgt.CasesMGT_2)
-	//AddP(mgt.CasesMGT_3)
-	//
-	////Parking
-	//AddP(parking.CasesParkingPass)
-	//
-	////WebAPI
-	//AddP(webapi.CasesWEBAPI)
-	//
-	////Revise
-	//AddAR(registry.CasesReviseGetTaskList)
-	////Resolve
-	//AddAR(registry.CasesResolveGetTaskList)
-	////Face
-	//AddP(face.CasesAuthWithFace)
-	//AddP(face.MetroComplexFaceID)
+	AddP(passes.CasesCancel)
 
+	//МТППК
+	AddP(mtppk.CasesMTPPKPasses)
+	AddP(mtppk.CasesMTPPK_MGT)
+	AddP(mtppk.CasesMTPPK_MCD_MO)
+	AddP(mtppk.CasesMTPPK_MCK)
+	AddP(mtppk.CasesMTPPK_MCD_MSK)
+	AddP(mtppk.CasesMTPPK_single)
+
+	//ММТС
+	AddP(mmts.CasesComplexPassMMTS1)
+	AddP(mmts.CasesComplexPassMMTS2)
+	AddP(mmts.CasesComplexPassMMTS3)
+	AddP(mmts.CasesComplexPassMMTS4)
+	AddP(mmts.CasesComplexPassMMTS5)
+
+	//MM
+	AddP(mm.CasesMetroComplexMM1)
+	AddP(mm.CasesMetroComplexMM2)
+	AddP(mm.CasesMetroComplexMM3)
+	AddP(mm.CasesMetroComplexMM4)
+
+	//МЦК
+	AddP(mck.CasesMetroComplexMCK1)
+	AddP(mck.CasesMetroComplexMCK2)
+	AddP(mck.CasesMetroComplexMCK3)
+	AddP(mck.CasesMetroComplexMCK4)
+
+	//МЦД
+	AddP(mcd.CasesMetroComplexMCDMSK1)
+	AddP(mcd.CasesMetroComplexMCDMSK2)
+	AddP(mcd.CasesMetroComplexMCDMSK3)
+	AddP(mcd.CasesMetroComplexMCDMSK4)
+	AddP(mcd.CasesMetroComplexMCDMSK5)
+	AddP(mcd.CasesComplexMCDMOPartOne)
+	AddP(mcd.CasesComplexMCDMOPartTwo)
+	AddP(mcd.CasesComplexMCDMOPartThree)
+	AddP(mcd.CasesComplexMCDMOPartFour)
+	AddP(mcd.CasesComplexMCDMOPartFife)
+
+	//Мобильная тройка
+	AddP(mt.CasesMetroComplexMT)
+
+	//APM
+	AddP(apm.CasesApmGateway)
+
+	//МГТ
+	AddP(mgt.CasesMGT_1)
+	AddP(mgt.CasesMGT_2)
+	AddP(mgt.CasesMGT_3)
+
+	//Parking
+	AddP(parking.CasesParkingPass)
+
+	//WebAPI
+	AddP(webapi.CasesWEBAPI)
+
+	//Revise
+	AddAR(registry.CasesReviseGetTaskList)
+	//Resolve
+	AddAR(registry.CasesResolveGetTaskList)
+	//Face
+	AddP(face.CasesAuthWithFace)
+	AddP(face.MetroComplexFaceID)
+
+	//Отмена оплаты в TWPG
 	AddP(twpg.CaseTWPGReverseOrder)
 
 	//черновики, которые пока что не запускаются

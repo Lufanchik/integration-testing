adf10582e767aaff80972572809db8b0183db9b6
metelkinav <metelkinav@multicarta.ru>
2020-07-17 12:07:22 +0300

    Virtual Troyka. Added online/offline authorisations

--- passes/mt/passes.go
+++ passes/mt/passes.go
@@ -12,12 +12,14 @@ var CasesMetroComplexMT = test.Cases{
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
@@ -26,12 +28,14 @@ var CasesMetroComplexMT = test.Cases{
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
@@ -40,12 +44,14 @@ var CasesMetroComplexMT = test.Cases{
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
@@ -54,107 +60,300 @@ var CasesMetroComplexMT = test.Cases{
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
 				PaymentType: test.PaymentTypePrepayed,
 				Carrier:     carriers.Carrier_MM,
 				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
 	{
-		N:        "6.ММТС-МЦК-ММ",
+		N:        "5.ММТС-МЦК-ММ",
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
 	{
-		N:        "7.МЦК-ММ-ММТС",
+		N:        "6.МЦК-ММ-ММТС",
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
 	{
-		N:        "8.МЦК-ММТС-ММ",
+		N:        "7.МЦК-ММТС-ММ",
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
 	{
-		N:        "9.МЦК-ММ-ММ",
+		N:        "8.МЦК-ММ-ММ",
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MMTS_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 		},
 	},
 	{
-		N:        "10.ММТС-ММ-ММ",
+		N:        "9.ММТС-ММ-ММ",
 		PassType: pass.PassType_PASS_MT,
 		T: test.T{
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MCK_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
 			},
 			&test.Pass{
-				Carrier:    carriers.Carrier_MM,
-				SubCarrier: carriers.SubCarrier_MM_SUB,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOnline,
+			},
+		},
+	},
+	{
+		N:        "10.MМ-MМ-2",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "11.МЦК-МЦК",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "12.ММТС-ММТС",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "13.ММ-МЦК-ММТС",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				PaymentType: test.PaymentTypePrepayed,
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "14.ММТС-МЦК-ММ",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "15.МЦК-ММ-ММТС",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "16.МЦК-ММТС-ММ",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "17.МЦК-ММ-ММ",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MMTS_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+		},
+	},
+	{
+		N:        "18.ММТС-ММ-ММ",
+		PassType: pass.PassType_PASS_MT,
+		T: test.T{
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MCK_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
+			},
+			&test.Pass{
+				Carrier:     carriers.Carrier_MM,
+				SubCarrier:  carriers.SubCarrier_MM_SUB,
+				RequestType: test.RequestTypeOffline,
 			},
 		},
 	},

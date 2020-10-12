d0bb99a7159a13ce1c7ce14bc2a0edd3c11aab4d
koznov <nkoznov@koznovnikita@gmail.com>
2020-10-12 11:13:38 +0300

    comment ingress

--- test/api.go
+++ test/api.go
@@ -157,7 +157,7 @@ func ValidatePass(t *testing.T, p *Pass, parent *Pass, ingress *Pass, isFirst bo
 
 	if ingress != nil {
 		expectPass.IngressId = ingress.id
-		expectPass.IngressCarrierTapId = ingress.carrierID
+		//expectPass.IngressCarrierTapId = ingress.carrierID
 	}
 
 	if p.isParent {

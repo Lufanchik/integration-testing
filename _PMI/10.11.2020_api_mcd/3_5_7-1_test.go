4fd9db47dc77c95b9bdc90adbde6bab915939e9d
its.crowd@gmail.com <its.crowd@gmail.com>
2020-11-17 05:29:24 +0300

    mcd

---
+++ _PMI/10.11.2020_api_mcd/3_5_7-1_test.go
@@ -0,0 +1,152 @@
+package mcd
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gavv/httpexpect"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"lab.dt.multicarta.ru/tp/common/messages/processing"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+// вход на МЦД МСК1, выход на МЦД МСК 2.
+func Test3_5_7_1(t *testing.T) {
+	pan := CardPan()
+	responseI := &processing.OnlinePassResponse{}
+	{
+		httpService := httpexpect.New(t, passUrl)
+		now := int(time.Now().UnixNano())
+
+		req := []byte(`{
+				"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(now) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(now) + `,
+				"resolution": 1,
+				"sign": "test",
+				"terminal": {
+					"id": "100",
+					"station": "2001060",
+					"direction": 1,
+					"sub_carrier": 6
+			},
+			"card": {
+					"system": 4,
+					"type": 2,
+					"pan": "` + pan + `",
+					"bin": 47617310,
+					"exp": "1224",
+					"emv": "` + CardEmvCorrect() + `",
+					"token": {
+					"type": 1
+				}
+			}
+		},
+		"auth": {
+			"sum": 4400,
+			"type": 1
+		}
+	}`)
+		fmt.Println(string(req))
+		request := &processing.OnlinePassRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(offlinePassMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), responseI)
+		require.NoError(t, err)
+
+		fmt.Println(responseI)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	{
+		httpService := httpexpect.New(t, passUrl)
+		now := int(time.Now().UnixNano())
+
+		req := []byte(`{
+				"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(now) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(now) + `,
+				"resolution": 1,
+				"sign": "test",
+				"terminal": {
+					"id": "100",
+					"station": "2001220",
+					"direction": 2,
+					"sub_carrier": 7
+			},
+			"card": {
+					"system": 4,
+					"type": 2,
+					"pan": "` + pan + `",
+					"bin": 47617310,
+					"exp": "1224",
+					"emv": "` + CardEmvCorrect() + `",
+					"token": {
+					"type": 1
+				}
+			}
+		},
+		"auth": {
+			"sum": 4400,
+			"type": 1
+		}
+	}`)
+		fmt.Println(string(req))
+		request := &processing.OnlinePassRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(offlinePassMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		response := &processing.OnlinePassResponse{}
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+		require.NoError(t, err)
+
+		fmt.Println(response)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	{
+		httpService := httpexpect.New(t, passUrl)
+		req := []byte(`{
+							"id": "` + responseI.Id + `"
+						}
+						`)
+		fmt.Println(string(req))
+		request := &processing.AuthRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(authStatusMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		response := &processing.AuthResponse{}
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+		require.NoError(t, err)
+
+		fmt.Println(response)
+	}
+}
---
+++ _PMI/10.11.2020_api_mcd/3_5_9-2_test.go
@@ -0,0 +1,180 @@
+package mcd
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gavv/httpexpect"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"lab.dt.multicarta.ru/tp/common/messages/processing"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+// вход в МЦД МО2, выход в МЦД МО1.
+func Test3_5_9_2(t *testing.T) {
+	pan := CardPan()
+	responseI := &processing.OnlinePassResponse{}
+	{
+		httpService := httpexpect.New(t, passUrl)
+		now := int(time.Now().UnixNano())
+
+		req := []byte(`{
+				"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(now) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(now) + `,
+				"resolution": 1,
+				"sign": "test",
+				"terminal": {
+					"id": "100",
+					"station": "2000460",
+					"direction": 1,
+					"sub_carrier": 9
+			},
+			"card": {
+					"system": 4,
+					"type": 2,
+					"pan": "` + pan + `",
+					"bin": 47617310,
+					"exp": "1224",
+					"emv": "` + MKEmulatorUnsuccessWithReauth() + `",
+					"token": {
+					"type": 1
+				}
+			}
+		},
+		"auth": {
+			"sum": 4400,
+			"type": 1
+		}
+	}`)
+		fmt.Println(string(req))
+		request := &processing.OnlinePassRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(onlinePassMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), responseI)
+		require.NoError(t, err)
+
+		fmt.Println(responseI)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	{
+		httpService := httpexpect.New(t, passUrl)
+		now := int(time.Now().UnixNano())
+
+		req := []byte(`{
+				"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(now) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(now) + `,
+				"resolution": 1,
+				"sign": "test",
+				"terminal": {
+					"id": "100",
+					"station": "2000055",
+					"direction": 2,
+					"sub_carrier": 8
+			},
+			"card": {
+					"system": 4,
+					"type": 2,
+					"pan": "` + pan + `",
+					"bin": 47617310,
+					"exp": "1224",
+					"emv": "` + MKEmulatorUnsuccessWithReauth() + `",
+					"token": {
+					"type": 1
+				}
+			}
+		},
+		"auth": {
+			"sum": 4400,
+			"type": 1
+		}
+	}`)
+		fmt.Println(string(req))
+		request := &processing.OnlinePassRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(onlinePassMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		response := &processing.OnlinePassResponse{}
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+		require.NoError(t, err)
+
+		fmt.Println(response)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	{
+		httpService := httpexpect.New(t, calculatorUrl)
+		now := int(time.Now().UnixNano())
+
+		req := []byte(`{
+				"pan": "` + pan + `",
+				"time": ` + strconv.Itoa(now) + `
+			}`)
+		fmt.Println(string(req))
+		request := &processing.CompleteWithCalculateRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(complete).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		response := &processing.CompleteWithCalculateResponse{}
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+		require.NoError(t, err)
+
+		fmt.Println(response)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	{
+		httpService := httpexpect.New(t, passUrl)
+		req := []byte(`{
+							"id": "` + responseI.Id + `"
+						}
+						`)
+		fmt.Println(string(req))
+		request := &processing.AuthRequest{}
+		err := json.Unmarshal(req, request)
+		require.NoError(t, err)
+
+		r := httpService.POST(authStatusMCD).WithJSON(request).
+			Expect().
+			Status(http.StatusOK)
+
+		logRequest(r)
+
+		response := &processing.AuthResponse{}
+		err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+		require.NoError(t, err)
+
+		fmt.Println(response)
+	}
+}

71ebd1b231b9b2702f9ac4211653f076fa06685f
its.crowd@gmail.com <its.crowd@gmail.com>
2021-03-20 00:59:54 +0300

    mcd fix

---
+++ aggregation/case8-direction/aggregation_test.go
@@ -0,0 +1,287 @@
+package case8_direction
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gavv/httpexpect"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"lab.dt.multicarta.ru/tp/common/messages/processing"
+	"lab.dt.multicarta.ru/tp/integration-testing/aggregation"
+	"lab.dt.multicarta.ru/tp/integration-testing/test"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+const (
+	passUrl = "http://localhost:9090"
+
+	onlinePassMM  = "/mm/twirp/sirocco.ProcessingAPI/ProcessOnlinePass"
+	onlinePassMCD = "/mcd/twirp/sirocco.ProcessingAPI/ProcessOnlinePass"
+)
+
+func logRequest(r *httpexpect.Response) {
+	trace := r.Header("X-Trace-ID")
+	fmt.Println(fmt.Sprintf("trace id: %s", trace.Raw()))
+}
+
+// мск 2000285
+// мо 2002952
+// виза 2
+// мир 4
+
+// 1 В пути с агрегацией
+// 2 В пути с без агрегации
+// 3 В пути, затем вход
+// 4 Выход, затем в пути
+// 5 В пути, 5 неопределенных проходов, в пути
+// 6 3 метки в пути подряд и 3 неопределенных прохода
+
+func Test_1(t *testing.T) {
+	{
+		now := time.Now()
+		pan := aggregation.Pan + "1"
+
+		reqMCD1 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 4,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 2,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		Request(t, reqMCD1, onlinePassMCD)
+	}
+}
+
+func Test_2(t *testing.T) {
+	{
+		now := time.Now()
+		pan := aggregation.Pan + "2"
+
+		reqMCD1 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 4,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 4,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		Request(t, reqMCD1, onlinePassMCD)
+	}
+}
+
+func Test_3(t *testing.T) {
+	{
+		now := time.Now()
+		pan := aggregation.Pan + "3"
+
+		reqMCD1 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 4,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 4,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		reqMCD2 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*7).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*7).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 1,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 4,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		Request(t, reqMCD1, onlinePassMCD)
+		Request(t, reqMCD2, onlinePassMCD)
+	}
+}
+
+func Test_4(t *testing.T) {
+	{
+		now := time.Now()
+		pan := aggregation.Pan + "4"
+
+		reqMCD2 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*5).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*5).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 2,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 4,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		reqMCD1 := []byte(`{
+			"id": "` + uuid.New().String() + `",
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+				"tap": {
+				"created": ` + strconv.Itoa(int(now.Add(time.Minute*6).UnixNano())) + `,
+					"resolution": 1,
+					"sign": "test",
+					"terminal": {
+						"id": "3213",
+						"station": "2000285",
+						"direction": 4,
+						"sub_carrier": 1
+					},
+				"card": {
+						"system": 4,
+						"type": 1,
+						"pan": "` + pan + `",
+						"bin": 47617310,
+						"exp": "1224",
+						"emv": "` + test.CardEmvCorrect() + `",
+						"token": {
+							"type": 1
+						}
+				}
+			},
+			"auth": {
+				"sum": 4600,
+				"type": 1
+			}
+		}`)
+
+		Request(t, reqMCD2, onlinePassMCD)
+		Request(t, reqMCD1, onlinePassMCD)
+	}
+}
+
+func Request(t *testing.T, req []byte, pe string) {
+	fmt.Println(string(req))
+	httpService := httpexpect.New(t, passUrl)
+
+	request := &processing.OnlinePassRequest{}
+	err := json.Unmarshal(req, request)
+	require.NoError(t, err)
+
+	r := httpService.POST(pe).WithJSON(request).
+		Expect().
+		Status(http.StatusOK)
+
+	logRequest(r)
+
+	response := &processing.OnlinePassResponse{}
+	err = jsonpb.Unmarshal(strings.NewReader(r.Body().Raw()), response)
+	require.NoError(t, err)
+
+	fmt.Println(response)
+}
